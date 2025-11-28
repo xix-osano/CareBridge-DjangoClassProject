c31ee2499c1609987aa6d2dd6c23dc05f5934f44
Enosh Osano Misonge <osanoenosh@gmail.com>
2025-11-29 01:17:18 +0300

    Added registration and login pages

--- careapp/urls.py
+++ careapp/urls.py
@@ -4,8 +4,8 @@ from careapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
-    path('', views.home, name='home'),
+
+    path('home/', views.home, name='home'),
     path('starter/', views.starter, name='starter'),
     path('about/', views.about, name='about'),
     path('services/', views.services, name='services'),
@@ -13,7 +13,9 @@ urlpatterns = [
     path('departments/', views.departments, name='departments'),
     path('doctors/', views.doctors, name='doctors'),
     path('show/', views.show, name='show'),
-    path('delete/<int:id>/', views.delete),
-    path('edit/<int:id>/', views.edit),
+    path('delete/<int:id>/', views.delete, name='delete'),
+    path('edit/<int:id>/', views.edit, name='edit'),
+    path('register/', views.register, name='register'),
+    path('', views.login_user, name='login'),
 
 ]
--- careapp/views.py
+++ careapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from careapp.models import *
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 
@@ -62,3 +64,53 @@ def edit(request, id):
         messages.success(request, "Appointment updated successfully.")
         return redirect("show")  # ensure url name matches
     return render(request, "edit.html", {"appointment": appointment})
+
+
+def register(request):
+    """ Show the registration form """
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+
+        # Check the password
+        if password == confirm_password:
+            try:
+                user = User.objects.create_user(username=username, password=password)
+                user.save()
+
+                # Display a message
+                messages.success(request, "Account created successfully")
+                return redirect('')
+            except:
+                # Display a message if the above fails
+                messages.error(request, "Username already exist")
+        else:
+            # Display a message saying passwords don't match
+            messages.error(request, "Passwords do not match")
+
+    return render(request, 'register.html')
+
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+
+        # Check if the user exists
+        if user is not None:
+            # login(request, user)
+            login(request,user)
+            messages.success(request, "You are now logged in!")
+            # Admin
+            if user.is_superuser:
+                return redirect('/appointment')
+
+            # For Normal Users
+            return redirect('home')
+        else:
+            messages.error(request, "Invalid login credentials")
+
+    return render(request, 'login.html')
